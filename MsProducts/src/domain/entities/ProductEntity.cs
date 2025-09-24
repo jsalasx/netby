b3e88521c36473b1417c0c0f74f074e87fbf489c
Salas Ladino, Jose Alejandro <jose.salas@sofka.com.co>
2025-09-24 01:55:22 -0500

    Initial

---
+++ MsProducts/src/domain/entities/ProductEntity.cs
@@ -0,0 +1,14 @@
+namespace MsProducts.Domain.Entities;
+
+public class ProductEntity
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string Category { get; set; } = null!;
+    public string ImageUri { get; set; } = null!;
+    public int Price { get; set; }
+    public int Stock { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
